466f2026a81b2e5fcb8c9501b3ca290a8b19a8a0
cochisjasu <145470097+cochisjasu@users.noreply.github.com>
2024-01-23 10:41:16 -0600

    despliegue 1

--- client-dev/src/env/environment.ts
+++ client-dev/src/env/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
     accessToken: 'pk.eyJ1IjoiZmVyY2hvMTgyZ29tZXoiLCJhIjoiY2t2YjRldTBwMHMwbjJwdDI3ODRqamphMSJ9.otkuEOs6FpAQPsGEEawRLg'
   },
   // base_url: "http://localhost:3030/api",
-  base_url: "http://164.90.254.102:3030/api",
+  // base_url: "http://164.90.254.102:3030/api",
+  base_url: "http://loopintermodal.com/api",
 }
